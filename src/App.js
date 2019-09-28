76c8aa34e4cccce829c6db32a2ead188fa729b6b
changbhc@gmail.com <changbhc@gmail.com>
2019-09-27 21:50:16 -0700

    trying to make the categories show properly

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Category from 'components/Category';
 import SideBar from 'components/SideBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-const useStyles = makeStyles({
-  root: {
-    display: 'flex',
-    marginTop: '3rem',
-  },
-})
+import 'App.scss';
 
 const categories = [
   'Brian',
@@ -19,12 +12,11 @@ const categories = [
 ];
 
 function App() {
-  const classes = useStyles();
   const [show, setShow] = useState(undefined);
   return (
-    <div className={classes.root}>
+    <div id="app">
       <SideBar/>
-      <div>
+      <div className="categories">
         {categories.map((category, index) => {
           return (
             <Category key={index} show={show} setShow={setShow} name={category}/>
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,27 +1,15 @@
 import React from 'react';
 import { Paper, Collapse } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import 'components/Category.scss';
 
-const useStyles = makeStyles({
-  root: {
-    width: '10rem',
-  },
-  title: {
-    display: 'flex',
-    justifyContent: 'center',
-  },
-})
-
 export default function Category(props) {
-  const classes = useStyles();
 
   return (
-    <Paper id='main' onMouseEnter={() => props.setShow(props.name)} onMouseLeave={() => props.setShow(undefined)}>
-      <div className={classes.title}>
+    <Paper id='category' onMouseEnter={() => props.setShow(props.name)} onMouseLeave={() => props.setShow(undefined)}>
+      <div className="title">
         <p>{props.name}</p>
       </div>
-      <Collapse in={props.show === props.name}>
+      <Collapse className="collapse" in={props.show === props.name}>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dignissim.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dignissim.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dignissim.</p>
